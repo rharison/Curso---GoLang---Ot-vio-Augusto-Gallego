5e8bf87ad40c343a67f04bfeb10b0b85c1e319b4
Rharison <rharison.abreu@gmail.com>
2025-07-13 19:50:25 -0300

    feat: funções com parâmetros variáticos

---
+++ 14 - Funções Avançadas/02 - Funções Variaticas/funcoes-variaticas.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func soma(numeros ...int) int {
+	total := 0
+
+	for _, numero := range numeros {
+		total += numero
+	}
+
+	return total
+}
+
+func escrever(texto string, numeros ...int) {
+	for _, numero := range numeros {
+		fmt.Println(texto, numero)
+	}
+}
+
+func main() {
+	totalSoma := soma(1, 2, 3, 4)
+
+	fmt.Println("Total: ", totalSoma)
+
+	escrever("Numero: ", 1, 2, 3)
+}
